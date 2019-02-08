dae70b75add9e0edd8cdc862e9d3c067f2401fb6
felcygrace <47312394+felcygrace@users.noreply.github.com>
2019-02-08 10:13:11 +0530

    Update one.java

--- one.java
+++ one.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 class OddEven{
 	public static void main (String[] arg){
-		int a=3;
+		Scanner Scan=new Scanner(System.in);
+		int a= Scan.nextInt();
 		if(a%2==0){
 			System.out.println("is even");
 		}
