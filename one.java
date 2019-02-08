590304a9fdfe25ff1412159dc71b0e612974a7d5
felcygrace <47312394+felcygrace@users.noreply.github.com>
2019-02-08 10:05:02 +0530

    Create one.java

---
+++ one.java
@@ -0,0 +1,11 @@
+class OddEven{
+	public static void main (String[] arg){
+		int a=3;
+		if(a%2==0){
+			System.out.println("is even");
+		}
+	    else{
+			System.out.println("is odd");
+		}
+	}
+}
